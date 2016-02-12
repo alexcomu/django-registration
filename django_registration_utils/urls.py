cbb4ff6325b34d5842b1f60131b5a74f654f46f0
alexcomu <alex.comunian@gmail.com>
2016-02-12 14:51:57 +0100

    added base template and login page

--- django_registration_utils/urls.py
+++ django_registration_utils/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^register/', include('registration.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}),
+    url('^', include('django.contrib.auth.urls'))
 ]
